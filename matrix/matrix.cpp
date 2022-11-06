183f59ade0c6386c14ced4b802f7fcc160bd079e
Mercurlc <alexanderarefievrus@gmail.com>
2022-11-06 14:02:39 +0300

    stringA

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -6,11 +6,11 @@ int main()
     float b[4][2] = { {1.20, 0.50}, {2.80, 0.40}, {5, 1}, {2.00, 1.50} };
     float c[3][2] = { {0,0}, {0,0}, {0,0} };
 
-    for (int strA = 0; strA < 3; strA++)
+    for (int stringA = 0; stringA < 3; stringA++)
     {
         for (int colB = 0; colB < 2; colB++)
             for (int strB = 0; strB < 4; strB++)
-                c[strA][colB] += a[strA][strB] * b[strB][colB];
+                c[stringA][colB] += a[stringA][strB] * b[strB][colB];
     }
 
 
